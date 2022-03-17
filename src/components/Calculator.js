ad0c412438a5e6508bceb055e18e86645cab5ae9
iLynette <lynetteacholah@icloud.com>
2022-03-17 18:10:07 +0300

    Move the logic directory from the component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import calculate from './logic/calculate';
+import calculate from '../logic/calculate';
 
 const Calculator = () => {
   const [state, setState] = useState({
