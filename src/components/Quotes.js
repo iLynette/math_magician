6f536f8eabdcc75fa65f416effc729a3650eb130
iLynette <lynetteacholah@icloud.com>
2022-03-22 14:00:36 +0300

    Add quotes

---
+++ src/components/Quotes.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Quotes extends Component {
+  render() {
+    return (
+      <div>
+          <p>
+            1. “All our dreams can come true if we have the courage to pursue them.” _Walt Disney
+          </p>
+          <p>
+            2. “If you hear a voice within you say ‘you cannot paint,’ then by all means paint and that voice will be silenced.” -Vincent Van Gogh
+          </p>
+          <p>
+              3. “It does not matter how slowly you go, so long as you do not stop.” -Confucius
+          </p>
+      </div>
+    )
+  }
+}
