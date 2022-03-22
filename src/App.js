9230580e983b0f00de5e0e18705f150041b3b1c1
iLynette <lynetteacholah@icloud.com>
2022-03-22 22:09:48 +0300

    Import all files on App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 /* eslint-disable react/prefer-stateless-function */
 
 import React from 'react';
+import Nav from './components/Nav';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
+import Quotes from './components/Quotes';
 
 class App extends React.Component {
   render() {
     return (
       <div className="Calculator">
+        <Nav />
+        <Home />
         <Calculator />
+        <Quotes />
       </div>
     );
   }
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React, { Component } from 'react';
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>Nv</div>
+    );
+  }
+}
