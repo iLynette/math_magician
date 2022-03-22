7d36df7c6ee606439ee1ca29714ec0f7f589264c
iLynette <lynetteacholah@icloud.com>
2022-03-22 22:26:09 +0300

    Display the nav items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,16 @@ import React, { Component } from 'react';
 export default class Nav extends Component {
   render() {
     return (
-      <div>Nv</div>
+      <nav className="navBar">
+        <h3>Math Magician</h3>
+        <div className="navItems">
+          <ul className="navMenu">
+            <li>Home</li>
+            <li>Calculator</li>
+            <li>Quotes</li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
