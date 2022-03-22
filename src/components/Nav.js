4fa07c94481d3d7c1400b897bfb1c5cd6bbabc7d
iLynette <lynetteacholah@icloud.com>
2022-03-22 23:53:00 +0300

    Style the navigation bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,11 +8,11 @@ export default class Nav extends Component {
     return (
       <nav className="navBar">
         <NavLink className="header" to="/">
-          <h3>Math Magician</h3>
+          <h3 className="title">Math Magicians</h3>
         </NavLink>
         <div className="navItems">
           <ul className="navMenu">
-            <li><NavLink className="homeLink" to="/">Home</NavLink></li>
+            <li><NavLink className="homeLink" to="/">Home </NavLink></li>
             <li><NavLink className="calculatorLink" to="/calculator">Calculator</NavLink></li>
             <li><NavLink className="quotesLink" to="/quotes">Quotes</NavLink></li>
           </ul>
