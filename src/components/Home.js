fa35007b6efa9bd47e0a0cd9651f6fcb4ee3b13a
iLynette <lynetteacholah@icloud.com>
2022-03-24 23:21:51 +0300

    Change

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ const Home = () => (
         On the other hand, we denounce with righteous indignation and dislike men who
         are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by
         desire, that they cannot foresee the pain and trouble ;
-        and equal blame belongs to those who fail in their duty through weakness of will.
+        to those who fail in their duty through weakness of will.
         as saying through shrinking from toil and pain.
         In a free hour, when our power of choice is untrammelled being able to do what we like best,
         every pleasure is to be welcomed
