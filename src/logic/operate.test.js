415e1f6de4a8bf8b43739de863072a53d401b9b0
iLynette <lynetteacholah@icloud.com>
2022-03-24 11:55:43 +0300

    Add unit tests

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -4,5 +4,17 @@ describe('operation functions', () => {
     test('division', () => {
         const result = operate('10', '2', '÷');
         expect(result).toBe('5')
+    });
+    test('addition', () => {
+        const result = operate('6', '2', '+');
+        expect(result).toBe('8')
+    })
+    test('subtraction', () => {
+        const result = operate('10', '5', '-');
+        expect(result).toBe('5')
+    })
+    test('multiplication', () => {
+        const result = operate('2', '2', 'x');
+        expect(result).toBe('4')
     })
 })
